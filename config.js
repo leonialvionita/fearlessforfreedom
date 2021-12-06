4f0939312041f6be9174e5349a4ad2c3b2427548
leonialvionita <91880107+leonialvionita@users.noreply.github.com>
2021-12-06 17:38:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,277 @@
+var config = {
+    style: 'mapbox://styles/leonialvionita/ckpifhyqe1qyd17rtcob4716c',
+    accessToken: 'pk.eyJ1IjoibGVvbmlhbHZpb25pdGEiLCJhIjoiY2twOG5uNW12MGE3bzJ2bXhxYjIzOWRvcCJ9.tgMJwbM9kn2DSprJ-_zYHg',
+    showMarkers: true,
+    markerColor: '#000000',
+    theme: 'dark',
+    use3dTerrain: false,
+    title: 'Menjelajahi Sebaran Lokasi Terbunuhnya 14 Jurnalis Indonesia Sejak Tahun 1996',
+    subtitle: '',
+    byline: '',
+    footer: 'Sumber Data : Aliansi Jurnalis Independen, Dewan Pers, Committee to Protect Journalists',
+    chapters: [
+        {
+            id: 'pembunuhan1',
+            alignment: 'left',
+            hidden: false,
+            title: 'MUHAMMAD JAMALUDDIN DITEMUKAN HANYUT DI SUNGAI',
+            description: 'Jamaluddin hilang sejak 20 Mei 2003 di Aceh. Ia ditemukan pada 17 Juni 2003 dengan kondisi hanyut terikat dan dan tidak bernyawa di sungai. Saat ditemukan, di tubuh Jamaluddin terdapat banyak luka. Pembunuhnya masih simpang siur apakah Gerakan Aceh Merdeka (GAM) atau TNI. Namun motifnya diduga karena liputan Jamaluddin tentang konflik Aceh yang memuncak karena diberlakukannya status Daerah Operasi Militer (DOM) di Aceh. <br> <br> <b>Tahun Kejadian:</b> 2003 <br> <b>Media Jurnalis:</b> TVRI Aceh <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [95.36893, 5.52609],
+              zoom: 9.53,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'pembunuhan2',
+            alignment: 'right',
+            hidden: false,
+            title: 'SORI ERSA SIREGAR TERTEMBAK DALAM GENCATAN SENJATA',
+            description: 'Pada 1 Juli 2003, Ersa bersama juru kameranya dilaporkan menghilang di Kuala Langsa. Mereka diculik oleh sekelompok pemuda Gerakan Aceh Merdeka (GAM). Di bulan Desember pada tahun yang sama, GAM menjanjikan Ersa bersama tiga tawanan lain akan dilepas dengan syarat TNI harus melakukan gencatan senjata dengan GAM selama dua hari. Pada 29 Desember, Ersa tewas dalam gencatan senjata di Simpang Ulim. Peluru mengenai kepala dan dada Ersa. Kepala Staf TNI AD saat itu mengakui bahwa peluru yang mengenai Ersa adalah peluru milik TNI. <br> <br> <b>Tahun Kejadian:</b> 2003 <br> <b>Media Jurnalis:</b> RCTI <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [97.50992, 4.96859],
+              zoom: 5.14,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan3',
+            alignment: 'left',
+            hidden: false,
+            title: 'HILANGNYA ELYUDIN TELAUMBAUNA',
+            description: 'Elyudin hilang sejak 29 Agustus 2005. Ia pamit pada istrinya akan meliput selama beberapa hari di Teluk Dalam. Elyudin membawa perlengakapan liputannya. Aliansi Jurnalis Independen (AJI) meyakini kasus hilangnya Elyudin adalah kasus pembunuhan. Sebelumnya, Elyudin memberitakan tentang konflik pemilihan umum kepala daerah di Nias Selatan. Selain itu, istri Elyudin juga mengingat pesan terakhir suaminya untuk mencari seorang tokoh publik di Nias Selatan jika terjadi apa-apa pada Elyudin. <br> <br> <b>Tahun Kejadian:</b> 2005 <br> <b>Media Jurnalis:</b> Berita Sore <br> <b>Status Kasus:</b> Dark Number / Hilang',
+            location: {
+              center: [97.80271, 0.62510],
+              zoom: 6.56,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan4',
+            alignment: 'right',
+            hidden: false,
+            title: 'PEMBUNUHAN NAIMULLAH',
+            description: 'Naimullah ditemukan tewas dibunuh di mobilnya yang terparkir di Pantai Penibungan. Kepala belakang dan pelipis kanannya pecah. Dua tangannya juga memar. Barang-barangnya hilang, termasuk tape recorder. Saat itu Naimullah sedang melakukan investigasi kasus pembalakan liar yang melibatkan anggota kepolisian di Kalimantan. <br> <br> <b>Tahun Kejadian:</b> 1997 <br> <b>Media Jurnalis:</b> Sinar Pagi <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [109.35168, 0.00460],
+              zoom: 5.60,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan5',
+            alignment: 'left',
+            hidden: false,
+            title: 'MUHAMMAD SYAIFULLAH DITEMUKAN MENINGGAL DI RUMAH KONTRAKANNYA',
+            description: 'Syaifullah ditemukan meninggal tergeletak terlentang dengan tangan memegang remote TV di rumah kontrakannya. TV-nya tidak menyala. Mulutnya keluar busa. Di dekat tubuhnya ada obat sakit kepala dan gelas bekas limun. Beberapa hari sebelumnya Syaifullah tidak bisa dihubungi. Syaifullah jurnalis yang kritis menulis dampak pertambangan batu bara di Kalimantan Selatan dan Kalimantan Timur. Sebelum menerima hasil otopsi, polisi menyatakan Syaifullah meninggal karena hipertensi. Setelahnya, polisi tidak mengumumkan hasil otopsi dan menutup penyidikan kasus. <br> <br> <b>Tahun Kejadian:</b> 2010 <br> <b>Media Jurnalis:</b> Kompas <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [116.86557, -1.23896],
+              zoom: 12.87,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan6',
+            alignment: 'right',
+            hidden: false,
+            title: 'FUAD MUHAMMAD SYAFRUDDIN DIBUNUH KARENA BERITA',
+            description: 'Udin dipukul dengan besi di bagian kepala dan perut hingga terkapar bersimbah darah di teras rumahnya oleh tamu tidak dikenal. Setelah tiga hari koma pasca pemukulan, Udin meninggal di Rumah Sakit Bethesda. Sebelumnya, Udin secara kritis menulis berita kasus suap dan korupsi yang melibatkan Pemda dan Bupati Bantul. <br> <br> <b>Tahun Kejadian:</b> 1996 <br> <b>Media Jurnalis:</b> Berita Nasional <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [110.35231, -7.91006],
+              zoom: 11.08,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan7',
+            alignment: 'left',
+            hidden: false,
+            title: 'HERLIYANTO TEWAS DENGAN LUKA SABETAN',
+            description: 'Herliyanto ditemukan tewas dengan sembilan luka sabetan senjata tajam di kawasan hutan jati Klenang Desa Tarokan, Probolinggo. Sebelumnya, Herliyanto kerap menulis secara kritis tentang kasus korupsi di Probolinggo. Salah satu tulisannya adalah tentang penyelewengan beras untuk rakyat miskin yang terjadi di Kecamatan Tiris, yang berhasil memenjarakan sang kepala desa. Terakhir, Herliyanto sedang memeriksa penyimpangan dana di Desa Tulupuri. Polisi sempat menangkap tiga orang tersangka pembunuhan Herliyanto, tetapi kemudian dibebaskan karena tidak cukup bukti dan salah satu pelaku dinyatakan sakit jiwa. <br> <br> <b>Tahun Kejadian:</b> 2006 <br> <b>Media Jurnalis:</b> Jember News <br> <b>Status Kasus:</b> Tersangka Bebas',
+            location: {
+              center: [113.33543, -7.82143],
+              zoom: 8.18,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan8',
+            alignment: 'right',
+            hidden: false,
+            title: 'ANAK AGUNG NARENDRA PRABANGSA DITEMUKAN TEWAS MENGAMBANG',
+            description: 'Prabangsa ditemukan tewas mengambang di laut Teluk Bungsil setelah sebelumnya dilaporkan hilang. Dahi Prabangsa remuk, pergelangan tangan kanannya patah, dan di lehernya terdapat luka lebam bekas jeratan tali. Dalang pembunuhan Prabangsa yang tengah dihukum seumur hidup adalah Nyoman Susrama. Susrama adalah pengawas proyek Dinas Pendidikan Bangli, Bali. Skandal korupsi proyek inilah yang sebelumnya diberitakan oleh Prabangsa. <br> <br> <b>Tahun Kejadian:</b> 2009 <br> <b>Media Jurnalis:</b> Radar Bali <br> <b>Status Kasus:</b> Pelaku Dihukum',
+            location: {
+              center: [115.50838, -8.52823],
+              zoom: 8.84,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan9',
+            alignment: 'left',
+            hidden: false,
+            title: 'AGUS MULYAWAN DITEMBAK PASCA REFERENDUM TIMOR TIMUR',
+            description: 'Agus Mulyawan tewas ditembak bersama delapan orang rombongan biarawati dan calon pastor di Los Palos pada 25 September 1999, tidak lama setelah referendum lepasnya Timor Timur dari Indonesia. Saat itu Agus yang ditugaskan untuk meliput oleh Asia Press sedang ikut rombongan biarawati yang hendak mengirim bantuan kemanusiaan pada pengungsi. Penembak adalah organisasi milisi binaan tentara Indonesia. Setelah ditembak dengan posisi di dalam mobil, mobil yang ditumpangi Agus dan rombongan dibakar, dan kemudian diceburkan ke Sungai Verukoco. <br> <br> <b>Tahun Kejadian:</b> 1999 <br> <b>Media Jurnalis:</b> Asia Press <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [127.00186, -8.49454],
+              zoom: 8.20,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan10',
+            alignment: 'right',
+            hidden: false,
+            title: 'ALFRETS MIRULEWAN DIPUKULI DAN DIBUANG DI PELABUHAN',
+            description: 'Alfrets tewas di bekas gudang setelah dipukuli sebanyak empat kali. Dua kali pemukulan di antaranya dilancarkan di bagian kepala Alfrets dengan menggunakan pipa. Setelahnya, Alfrets dibuang ke pelabuhan oleh empat pelaku dengan menggunakan sampan nelayan, hingga kemudian ditemukan mengambang di Pelabuhan Pantai Wonreli, Pulau Kisar, Maluku Tenggara Barat. Salah satu pelaku adalah aparat kepolisian. Sebelumnya, Alfrets yang adalah Pemimpin Redaksi Mingguan Pelangi Maluku sedang menginvestigasi kasus penyelundupan BBM yang menyebabkan kelangkaan BBM di Pulau Kisar. <br> <br> <b>Tahun Kejadian:</b> 2010 <br> <b>Media Jurnalis:</b> Tabloid Pelangi <br> <b>Status Kasus:</b> Penyidikan Berhenti',
+            location: {
+              center: [127.18188, -8.06513],
+              zoom: 11.34,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan11',
+            alignment: 'left',
+            hidden: false,
+            title: 'TEWASNYA MUHAMMAD SAYUTI BOCHARI',
+            description: 'Sayuti meninggal di rumah sakit dengan luka di bagian kepala dan leher. Sebelumnya Sayuti ditemukan tidak sadarkan diri di jalan raya Kabupaten Luwu. Motornya ditemukan di sebelah Sayuti dalam kondisi tidak rusak. Keluarga dan teman-teman Sayuti mengatakan luka Sayuti mengindikasikan ia telah dipukuli. Sebelumnya, Sayuti menulis beberapa artikel tentang Pemda yang menggelapkan dana pengentasan kemiskinan dan pencurian kayu yang melibatkan Kepala Daerah. Pemimpin Redaksi Pos Makassar mengatakan kematian Sayuti berkaitan dengan pemberitaannya tentang korupsi daerah. Sedangkan kepolisian mengatakan kematian Sayuti dikarenakan kecelakaan lalu lintas. <br> <br> <b>Tahun Kejadian:</b> 1997 <br> <b>Media Jurnalis:</b> Pos Makassar <br> <b>Status Kasus:</b> -',
+            location: {
+              center: [120.23043, -3.20490],
+              zoom: 6.56,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan12',
+            alignment: 'right',
+            hidden: false,
+            title: 'RIDWAN SALAMUN TEWAS DIKEROYOK',
+            description: 'Ridwan dibiarkan terkapar selama dua jam di jalan raya Kota Tual, Maluku Tenggara, dan kemudian tewas sesampainya di rumah sakit. Ridwan dikeroyok massa menggunakan parang dan pipa ketika meliput kerusuhan antarwarga Desa Fiditan dan Dusun Banda Eli. Terdapat luka bacok di leher dan punggungnya. Setelah meninggal, Ridwan sempat dijadikan tesangka oleh Polda Maluku karena diduga terlibat dalam konflik. Tiga pelaku pengeroyokan Ridwan sempat divonis bebas murni pada 2011, sebelum kemudian dalam putusan kasasi empat pelaku dihukum empat tahun penjara. <br> <br> <b>Tahun Kejadian:</b> 2010 <br> <b>Media Jurnalis:</b> Sun TV <br> <b>Status Kasus:</b> Pelaku Divonis 4 Tahun',
+            location: {
+              center: [132.74715, -5.61899],
+              zoom: 6.35,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan13',
+            alignment: 'left',
+            hidden: false,
+            title: 'LEIRON KOGOYA TEWAS TERTEMBAK DI PESAWAT',
+            description: 'Leiron tewas tertembak di bagian leher setelah pesawat yang ia tumpangi, Trigana Air, ditembaki oleh kelompok tidak dikenal ketika akan mendarat di Bandar Udara Mulia. Perjalanan Leiron dari Nabire ke Mulia awalnya untuk menengok keluarganya. Namun pemimpin Papua Pos Nabire meminta Leiron untuk membantu liputan pemilihan kepala daerah di Puncak Jaya. <br> <br> <b>Tahun Kejadian:</b> 2012 <br> <b>Media Jurnalis:</b> Pos Papua Nabire <br> <b>Status Kasus:</b> -',
+            location: {
+              center: [137.96504, -3.73215],
+              zoom: 7.00,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'pembunuhan14',
+            alignment: 'right',
+            hidden: false,
+            title: 'ARDIANSYAH MATRAIS WIBISONO DIBUANG KE LAUT HIDUP-HIDUP',
+            description: 'Setelah hilang selama dua hari, Ardiansyah ditemukan tewas mengapung di Sungai Maro. Kematian Ardiansyah sempat dinyatakan sebagai bunuh diri oleh Polres Merauke. Namun setelah otopsi, Mabes Polri menyatakan Ardiansyah tewas akibat penganiayaan. Investigasi AJI menemukan bahwa wajah Ardiansyah membengkak, telinganya terus mengeluarkan darah, giginya rompal, di lehernya ada bekas jeratan, dan di beberapa tubuhnya ada bekas pukulan benda tumpul. Ardiansyah kemudian dibuang ke laut hidup-hidup. Sebelumnya, sejumlah wartawan di Merauke mendapat teror ancaman pembunuhan berupa telepon dan SMS dari orang tidak dikenal, pasca pemberitaan media massa di Merauke tentang aksi perusakan baliho dari salah satu pasangan calon bupati dan wakil bupati menjelang Pilkada setempat. Ardiansyah juga pernah meliput tentang pembalakan liar, yang mana setelahnya ia merasa ada yang mengikutinya sepulang kerja. <br> <br> <b>Tahun Kejadian:</b> 2010 <br> <b>Media Jurnalis:</b> Tabloid Jubi <br> <b>Status Kasus:</b> Dark Number',
+            location: {
+              center: [140.38546, -8.47727],
+              zoom: 13.61,
+              pitch: 45.00,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+    ]
+};
